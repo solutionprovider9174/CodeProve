380142dae7e640badf765303e97d99c2ccb5ddbb
webexpert <pksalo6@gmail.com>
2020-10-09 10:20:57 +0530

    forgot password

--- CodeProve/settings.py
+++ CodeProve/settings.py
@@ -127,13 +127,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dinkyaviox@gmail.com'
-EMAIL_HOST_PASSWORD = 'aviox@dinky2020'
+EMAIL_HOST_USER = 'deepakpandayaviox@gmail.com'
+EMAIL_HOST_PASSWORD = 'deep@mail.com'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'dinkyaviox@gmail.com'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- CodeProve/urls.py
+++ CodeProve/urls.py
@@ -52,11 +52,11 @@ urlpatterns = [
             }
          ),
          name='logout'),
-    url(r'^password_reset/$', views.PasswordResetView.as_view(template_name='app/password_reset_form.html'), name='password_reset'),
-    url(r'^password_reset/done/$', views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path('password-reset/', views.PasswordResetView.as_view(template_name='app/password_reset_form.html'), name='password_reset'),
+    path('password-reset/done/', views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
+    path('passwor-reset-confirm/<uidb64>/<token>/',
         views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html'), name='password_reset_confirm'),
-    url(r'^reset/done/$', views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset-complete/', views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
